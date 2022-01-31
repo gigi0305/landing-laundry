58b6f87d173f99df48a4b1a8c453baf314a6d1ab
gigi0305 <luigi35santoro@gmail.com>
2022-01-31 14:22:54 +0100

    landing-setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Bar from './components/navbar';
+import Cards from './components/cards';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Bar />
       </header>
+      <div className='cover'>
+        <Cards/>
+      </div>
     </div>
   );
 }
